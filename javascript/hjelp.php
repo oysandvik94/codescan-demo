88081d6416617a2fc2a62351623afc2d75d7a2a5
Øystein Sandvik <oysandvik94@gmail.com>
2022-10-02 21:38:41 +0200

    Add vuknerable packages

--- javascript/hjelp.php
+++ javascript/hjelp.php
@@ -59,4 +59,66 @@ $app->post('/login', function(Silex\Application $app) {
     omg_important_private_functionality_here();
 });
 
+<?
+$a = htmlentities($_GET['a']);
+$b = $_GET['b'];
+$c = $_GET['c'];
+$d = htmlentities($b);
+
+echo ($a); // safe
+echo (htmlentities($b)); // safe
+echo ($c); // XSS vulnerability
+echo ($d); // safe
+echo (htmlentities($_GET['id']); // safe
+?>
+
+<html>
+<? $name = $_GET['name']; // Comment ?><? echo($name); // XSS 1 ?>
+<script>
+document.write('<? echo($_GET['city']); // XSS 2 ?>');
+</script>
+</html>
+
+<html>
+<?php
+$name = $_GET['name'];
+$msg = 'Welcome '.$name;
+?>
+<head>
+<title><? echo($name); /* XSS 1 */ ?></title>
+</head>
+<body>
+<? echo($msg); /* XSS 2 */ ?>
+</body>
+</html>
+
+<?
+$name = $_GET['name'];
+?>
+<?=$_GET['name']; //XSS 1 ?>
+<?=$name // XSS 2 ?>
+
+<html>
+<script language="php">
+$d = $_GET['d'];
+echo($d); // XSS
+</script>
+</html>
+
+<?
+echo($_GET['name']); // XSS 1
+echo($_POST['name']); // XSS 2
+echo($_REQUEST['name']); // XSS 3
+?>
+
+<?
+$username = $_GET['username'];
+$result=mysql_query('SELECT * FROM users WHERE username="'.$username.'"');
+?>
+
+<?
+$cmd = $_GET['command'];
+passthru('SomeApp.exe '.$cmd);
+?>
+
 $app->run();
